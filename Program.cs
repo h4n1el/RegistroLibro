a77d03fd7152366aca701a1bc2027ae725feb917
h4n1el <66563648+h4n1el@users.noreply.github.com>
2026-09-20 11:19:23 -0400

    arreglando los services y su llamada en program

--- Program.cs
+++ Program.cs
@@ -3,42 +3,33 @@ using RegistroLibros.DAL;
 using Microsoft.EntityFrameworkCore;
 using RegistroLibros.Services;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-    builder.Services.AddDbContextFactory<LibrosDbContext>(options =>
-    options.UseSqlServer(builder.Configuration["ConStr"]));
-
-    builder.Services.AddScoped<LibroServices>();
-    builder.Services.AddScoped<EstudianteServices>();
-    
-    builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-    
-
-    
+builder.Services.AddDbContextFactory<BibliotecaDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConStr")));
 
 builder.Services.AddScoped<LibroRepository>();
+builder.Services.AddScoped<LibroServices>();
 
+builder.Services.AddScoped<EstudianteRepository>();
+builder.Services.AddScoped<EstudianteServices>();
 
 var app = builder.Build();
-
-
 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     app.UseHsts();
 }
+
+app.UseHttpsRedirection();
 app.UseAntiforgery();
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
 
 app.Run();
